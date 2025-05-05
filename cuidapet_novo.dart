1f7f75e5afccc9dc75dd283bad3a1423ad59cebd
victoria caixeta <victoriarabelocaixeta28@gmail.com>
2025-05-05 14:53:41 -0300

    primeiro precisa apertar em 8 para adicionar ao carrinho, depois selecionar o código do produto

--- cuidapet_novo.dart
+++ cuidapet_novo.dart
@@ -27,7 +27,9 @@ void cuidapetrestrito() {
         if (met == '1') {
           valorFinal *= 0.9;
           print("O pagamento em dinheiro possui um desconto de 10%.");
-          print("Total a pagar (com desconto): R\$${valorFinal.toStringAsFixed(2)}");
+          print(
+            "Total a pagar (com desconto): R\$${valorFinal.toStringAsFixed(2)}",
+          );
         } else {
           print("Total a pagar: R\$$valorFinal");
         }
@@ -39,7 +41,9 @@ void cuidapetrestrito() {
       case '2':
         print("\n========== RELATÓRIO DE VENDAS ==========");
         print("Quantidade de vendas: $quantidadeVendas");
-        print("Valor total arrecadado: R\$${valorTotalVendas.toStringAsFixed(2)}");
+        print(
+          "Valor total arrecadado: R\$${valorTotalVendas.toStringAsFixed(2)}",
+        );
         print("=========================================\n");
         break;
 
@@ -87,79 +91,105 @@ void main() {
 
       switch (opcao) {
         case '1':
-          print("Código 101 - Ração Royal Canin Indoor Para Cães Adultos De Porte Mini De Ambientes Internos 7,5kg na promoção pelo preço de R\$290,00.");
-          print("Código 102 - Ração Royal Canin Sterilised para Gatos Adultos Castrados e com o valor promocional de R\$492,00.");
-          print("Código 103 - Bifinho Keldog para Cães Porte Pequeno Sabor Carne e Cereais por R\$23,92.");
-          print("Código 104 - Fraldas Descartáveis Super Secão para Cães Machos com 12 Unidades R\$38,61.");
-          print("Escolha o código do produto desejado (ou 0 para voltar): ");
+          print(
+            "Código 101 - Ração Royal Canin Indoor Para Cães Adultos De Porte Mini De Ambientes Internos 7,5kg na promoção pelo preço de R\$290,00.",
+          );
+          print(
+            "Código 102 - Ração Royal Canin Sterilised para Gatos Adultos Castrados e com o valor promocional de R\$492,00.",
+          );
+          print(
+            "Código 103 - Bifinho Keldog para Cães Porte Pequeno Sabor Carne e Cereais por R\$23,92.",
+          );
+          print(
+            "Código 104 - Fraldas Descartáveis Super Secão para Cães Machos com 12 Unidades R\$38,61.",
+          );
+          print("Código 8 - Adicionar produto ao carrinho.");
+          print("Código 0 - Voltar): ");
           var codigo = stdin.readLineSync()!;
 
           if (codigo == '0') break;
 
-          if (carrinho.length < 3) {
-            switch (codigo) {
-              case '101':
-                carrinho.add("Ração Royal Canin Indoor Para Cães Adultos De Porte Mini De Ambientes Internos 7,5kg");
-                preco.add(290.00);
-                print("Produto adicionado.");
-                break;
-              case '102':
-                carrinho.add("Ração Royal Canin Sterilised para Gatos Adultos Castrados");
-                preco.add(492.00);
-                print("Produto adicionado.");
-                break;
-              case '103':
-                carrinho.add("Bifinho Keldog para Cães Porte Pequeno Sabor Carne");
-                preco.add(23.92);
-                print("Produto adicionado.");
-                break;
-              case '104':
-                carrinho.add("Fraldas Descartáveis Super Secão para Cães Machos");
-                preco.add(38.61);
-                print("Produto adicionado.");
-                break;
-              default:
-                print("Código inválido.");
-            }
-          } else {
-            print("Seu carrinho já está cheio. (Máximo de 3 itens).");
-          }
-          break;
+          if (codigo == '8') {
+            print("Escolha o código do produto desejado: ");
+            codigo = stdin.readLineSync()!;
 
+            if (carrinho.length < 3) {
+              switch (codigo) {
+                case '101':
+                  carrinho.add(
+                    "Ração Royal Canin Indoor Para Cães Adultos De Porte Mini De Ambientes Internos 7,5kg",
+                  );
+                  preco.add(290.00);
+                  print("Produto adicionado.");
+                  break;
+                case '102':
+                  carrinho.add(
+                    "Ração Royal Canin Sterilised para Gatos Adultos Castrados",
+                  );
+                  preco.add(492.00);
+                  print("Produto adicionado.");
+                  break;
+                case '103':
+                  carrinho.add(
+                    "Bifinho Keldog para Cães Porte Pequeno Sabor Carne",
+                  );
+                  preco.add(23.92);
+                  print("Produto adicionado.");
+                  break;
+                case '104':
+                  carrinho.add(
+                    "Fraldas Descartáveis Super Secão para Cães Machos",
+                  );
+                  preco.add(38.61);
+                  print("Produto adicionado.");
+                  break;
+                default:
+                  print("Código inválido.");
+              }
+            } else {
+              print("Seu carrinho já está cheio. (Máximo de 3 itens).");
+            }
+            break;
+          }
         case '2':
           print("Código 201 - Banho e tosa - R\$55,99.");
           print("Código 202 - Tosa higienica - R\$12,99.");
           print("Código 203 - Hidratação dos pelos - R\$20,99.");
-          print("Escolha o código do serviço desejado (ou 0 para voltar): ");
+          print("Código 8 - Adicionar produto ao carrinho.");
+          print("Código 0 - Voltar: ");
           var codigoServico = stdin.readLineSync()!;
 
           if (codigoServico == '0') break;
 
-          if (carrinho.length < 3) {
-            switch (codigoServico) {
-              case '201':
-                carrinho.add("Banho e Tosa");
-                preco.add(55.99);
-                print("Serviço adicionado.");
-                break;
-              case '202':
-                carrinho.add("Tosa Higienica");
-                preco.add(12.99);
-                print("Serviço adicionado.");
-                break;
-              case '203':
-                carrinho.add("Hidratação dos pelos");
-                preco.add(20.99);
-                print("Serviço adicionado.");
-                break;
-              default:
-                print("Código inválido.");
-            }
-          } else {
-            print("Seu carrinho já está cheio. (Máximo de 3 itens).");
-          }
-          break;
+          if (codigoServico == '8') {
+            print("Escolha o código do serviço desejado: ");
+            codigoServico = stdin.readLineSync()!;
 
+            if (carrinho.length < 3) {
+              switch (codigoServico) {
+                case '201':
+                  carrinho.add("Banho e Tosa");
+                  preco.add(55.99);
+                  print("Serviço adicionado.");
+                  break;
+                case '202':
+                  carrinho.add("Tosa Higienica");
+                  preco.add(12.99);
+                  print("Serviço adicionado.");
+                  break;
+                case '203':
+                  carrinho.add("Hidratação dos pelos");
+                  preco.add(20.99);
+                  print("Serviço adicionado.");
+                  break;
+                default:
+                  print("Código inválido.");
+              }
+            } else {
+              print("Seu carrinho já está cheio. (Máximo de 3 itens).");
+            }
+            break;
+          }
         case '3':
           print("\nCarrinho de Compras:");
           for (var i = 0; i < carrinho.length; i++) {
